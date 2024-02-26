1e850e61aad2f0838f140383e0c6225a295a0f8c
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-02-26 20:12:19 +0100

    Added Shift1D, 2D and 3D

---
+++ src/Rephidock.AtomicAnimations/Rephidock.AtomicAnimations/Shift1D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Rephidock.GeneralUtilities;
+using Rephidock.AtomicAnimations.Base;
+
+
+namespace Rephidock.AtomicAnimations;
+
+
+/// <summary>
+/// Shifts 1 value over time.
+/// Is not an excusilve ease.
+/// </summary>
+public class Shift1D : Ease {
+
+	readonly float shift;
+	float accumulator;
+	readonly Action<float> adder;
+
+	/// <inheritdoc cref="Shift1D"/>
+	public Shift1D(
+		float shift,
+		TimeSpan duration,
+		EasingCurve easingCurve,
+		Action<float> adder
+	)
+	: base(duration, easingCurve) {
+		this.shift = shift;
+		this.accumulator = 0;
+		this.adder = adder;
+	}
+
+	/// <inheritdoc/>
+	protected override void EaseUpdateImpl(float valueProgressNew) {
+		float newValue = MoreMath.Lerp(0, shift, valueProgressNew);
+		adder(newValue - accumulator);
+		accumulator = newValue;
+	}
+
+}
