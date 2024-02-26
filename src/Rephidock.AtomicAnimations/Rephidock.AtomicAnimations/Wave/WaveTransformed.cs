2563571bed145c8ce6f7f279f5bb9434ee54ef36
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-02-26 21:42:41 +0100

    Tiny doc fix

--- src/Rephidock.AtomicAnimations/Rephidock.AtomicAnimations/Wave/WaveTransformed.cs
+++ src/Rephidock.AtomicAnimations/Rephidock.AtomicAnimations/Wave/WaveTransformed.cs
@@ -11,10 +11,11 @@ namespace Rephidock.AtomicAnimations.Wave;
 /// </summary>
 public readonly struct WaveTransformed {
 
-	/// <summary>The wave that is to be moved and stretched.</summary>
 #if NET8_0_OR_GREATER
+	/// <summary>The wave that is to be moved and stretched.</summary>
 	public required Wave Wave { get; init; }
 #else
+	/// <summary>The wave that is to be moved and stretched.</summary>
 	/// <remarks>Required.</remarks>
 	public /* required */ Wave Wave { get; init; }
 #endif
