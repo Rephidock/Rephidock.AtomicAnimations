e7d3174f844a223d80aeb211e8f94e199ca58e8b
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-02-26 21:52:37 +0100

    Create AnimationPlayer.cs

---
+++ src/Rephidock.AtomicAnimations/Rephidock.AtomicAnimations/AnimationPlayer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Rephidock.AtomicAnimations.Base;
+
+
+namespace Rephidock.AtomicAnimations;
+
+
+/// <summary>
+/// <para>
+/// A player that automatically plays given animations.
+/// Animations are started the moment they are added
+/// and forgotten the moment they are finished.
+/// Multiple animations can be played at the same time.
+/// </para>
+/// </summary>
+/// <remarks>
+/// Animations are updated in the order of addition.
+/// </remarks>
+public class AnimationPlayer {
+
+	readonly LinkedList<Animation> animations = new();
+
+	/// <inheritdoc cref="Run(Animation, TimeSpan)"/>
+	public void Run(Animation animation) => Run(animation, TimeSpan.Zero);
+
+	/// <summary>
+	/// Adds an animation to play.
+	/// The animation is played immediately.
+	/// Stores a reference to the animation until it ends.
+	/// </summary>
+	public void Run(Animation animation, TimeSpan initialTime) {
+		animation.StartAndUpdate(initialTime);
+		animations.AddLast(animation);
+	}
+
+	/// <summary>
+	/// Updates all currently playing animations with time since last update.
+	/// </summary>
+	/// <remarks>
+	/// Negative <paramref name="deltaTime"/> has undefined behavior.
+	/// </remarks>
+	public void Update(TimeSpan deltaTime) {
+
+		LinkedListNode<Animation>? nextNode;
+		LinkedListNode<Animation>? currentNode = animations.First;
+
+		// Update all animations
+		while (currentNode is not null) {
+
+			// Update and find next
+			currentNode.Value.Update(deltaTime);
+			nextNode = currentNode.Next;
+
+			// Remove node with finished animation
+			if (currentNode.Value.HasEnded) {
+				animations.Remove(currentNode);
+			}
+
+			// Continue to the next node
+			currentNode = nextNode;
+		}
+
+	}
+
+	/// <summary>
+	/// <para>
+	/// Halts and clears all given animations,
+	/// effectively halting and forgetting them.
+	/// </para>
+	/// </summary>
+	public void HaltAndClear() {
+
+		// Halt all animations
+		foreach (var animation in animations) {
+			animation.Halt();
+		}
+
+		// Clear references
+		animations.Clear();
+	}
+
+}
