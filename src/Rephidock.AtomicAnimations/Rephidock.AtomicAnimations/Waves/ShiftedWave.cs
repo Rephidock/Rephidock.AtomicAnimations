d79e1469af085dd39d226dea7b2d9ea0b60cdd90
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-07 23:39:13 +0200

    Create ShiftedWave.cs

---
+++ src/Rephidock.AtomicAnimations/Rephidock.AtomicAnimations/Waves/ShiftedWave.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Rephidock.AtomicAnimations.Waves;
+
+
+/// <summary>
+/// A <see cref="Waves.Wave"/> that is horizontally shifted.
+/// </summary>
+public readonly struct ShiftedWave {
+
+#if NET8_0_OR_GREATER
+	/// <summary>The wave that is to be moved and stretched.</summary>
+	public required Wave Wave { get; init; }
+#else
+	/// <summary>The wave that is to be moved and stretched.</summary>
+	/// <remarks>Required.</remarks>
+	public /* required */ Wave Wave { get; init; }
+#endif
+
+	/// <summary>The horizontal offset of the wave</summary>
+	public float Offset { get; init; }
+
+	/// <inheritdoc cref="Wave.GetValueAt(float)"/>
+	public float GetValueAt(float horizontalPosition) {
+		return Wave.GetValueAt(horizontalPosition - Offset);
+	}
+
+}
