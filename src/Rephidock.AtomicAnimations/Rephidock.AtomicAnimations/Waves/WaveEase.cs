aaea28cb68cbe697f6721542221215cb57786d19
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-08 00:02:10 +0200

    Reimplemented WaveEase

---
+++ src/Rephidock.AtomicAnimations/Rephidock.AtomicAnimations/Waves/WaveEase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Rephidock.GeneralUtilities;
+using Rephidock.AtomicAnimations.Base;
+
+
+namespace Rephidock.AtomicAnimations.Waves;
+
+
+/// <summary>
+/// Shifts a <see cref="Wave"/> horizontally over time.
+/// Is an exclusive ease.
+/// </summary>
+public class WaveEase : Ease {
+
+	readonly float startOffset;
+	readonly float endOffset;
+	readonly Wave baseWave;
+	readonly Action<ShiftedWave> updater;
+
+	/// <inheritdoc cref="WaveEase"/>
+	public WaveEase(
+		Wave wave,
+		float startOffset,
+		float endOffset,
+		TimeSpan duration,
+		EasingCurve easingCurve,
+		Action<ShiftedWave> updater
+	)
+	: base(duration, easingCurve) {
+		this.startOffset = startOffset;
+		this.endOffset = endOffset;
+		this.baseWave = wave;
+		this.updater = updater;
+	}
+
+	/// <inheritdoc/>
+	protected override void EaseUpdateImpl(float valueProgressNew) {
+
+		// Create a shifted wave
+		var shiftedWave = new ShiftedWave() {
+			Wave = baseWave,
+			Offset = MoreMath.Lerp(startOffset, endOffset, valueProgressNew),
+		};
+
+		// Update
+		updater(shiftedWave);
+
+	}
+
+}
