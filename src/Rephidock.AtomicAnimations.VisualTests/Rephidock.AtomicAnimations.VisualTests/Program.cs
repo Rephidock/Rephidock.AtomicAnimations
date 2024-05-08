a6df0b0e16940ed9e9263fbdae72dc41753a8212
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-08 22:39:45 +0200

    Created a test project

---
+++ src/Rephidock.AtomicAnimations.VisualTests/Rephidock.AtomicAnimations.VisualTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+
+namespace Rephidock.AtomicAnimations.VisualTests;
+
+
+internal class Program {
+
+
+	static void Main(string[] args) {
+
+		
+		Console.WriteLine("Hello, World!");
+
+		RenderWindow window = new(new VideoMode(800, 600), "Atomic Animations Visual Tests");
+		window.Closed += (_, _) => window.Close();
+
+
+		while (window.IsOpen) {
+
+			window.DispatchEvents();
+
+			window.Clear(Color.Black);
+			window.Display();
+
+		}
+
+		window.Dispose();
+       
+	}
+
+}
