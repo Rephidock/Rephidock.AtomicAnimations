cae5696f90b4655d3dc37083d611755d0649352d
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-12 00:02:09 +0200

    Create TextExplorerTimeFlow.cs

---
+++ src/Rephidock.AtomicAnimations.VisualTests/Rephidock.AtomicAnimations.VisualTests/Tests/TextExplorerTimeFlow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SFML.System;
+
+
+namespace Rephidock.AtomicAnimations.VisualTests.Tests;
+
+
+[VisualTestMeta(Name = "Explorer Time Flow")]
+public class TextExplorerTimeFlow : VisualTest {
+
+	TimeSpan currentTime;
+
+	public override void Start(TimeSpan startTime) {
+		currentTime = startTime;
+	}
+
+	public override void Update(TimeSpan deltaTime) {
+		currentTime += deltaTime;
+	}
+
+	public override void Draw(Drawer drawer) {
+		drawer.DrawText(currentTime.ToString(), new Vector2f(100, 100));
+	}
+
+}
