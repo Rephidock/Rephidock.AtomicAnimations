4898bdf56eaa5193ce5e9ec5c1f92d235dbc8e12
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-19 01:16:36 +0200

    ExplorerEvents test: Added event count

--- src/Rephidock.AtomicAnimations.VisualTests/Rephidock.AtomicAnimations.VisualTests/Tests/ExplorerEvents.cs
+++ src/Rephidock.AtomicAnimations.VisualTests/Rephidock.AtomicAnimations.VisualTests/Tests/ExplorerEvents.cs
@@ -11,6 +11,8 @@ public class ExplorerEvents : VisualTest {
 	ArrowDirection? lastArrowDirection = null;
 	int? lastNumericEvent = null;
 
+	int eventsRegistered = 0;
+
 	public override void Start(TimeSpan startTime) { }
 
 	public override void Update(TimeSpan deltaTime) { }
@@ -20,12 +22,25 @@ public class ExplorerEvents : VisualTest {
 		string lastDirectionalString = lastArrowDirection.HasValue ? lastArrowDirection.Value.ToString() : "None";
 		string lastNumericString = lastNumericEvent.HasValue ? lastNumericEvent.Value.ToString() : "None";
 
-		drawer.DrawText($"Last Directional: {lastDirectionalString}", new Vector2f(100, 100));
-		drawer.DrawText($"Last Numeric: {lastNumericString}", new Vector2f(100, 100 + drawer.MainFontLineSpacing));
+		Vector2f currentPosition = new(80, 100);
+
+		drawer.DrawText($"Last Directional: {lastDirectionalString}", currentPosition);
+		currentPosition.Y += drawer.MainFontLineSpacing;
+
+		drawer.DrawText($"Last Numeric: {lastNumericString}", currentPosition);
+		currentPosition.Y += drawer.MainFontLineSpacing;
+
+		drawer.DrawText($"Event count: {eventsRegistered}", currentPosition);
 	}
 
-	public override void HandleDirectionEvent(ArrowDirection @event) => lastArrowDirection = @event;
+	public override void HandleDirectionEvent(ArrowDirection @event) {
+		lastArrowDirection = @event;
+		eventsRegistered++;
+	}
 
-	public override void HandleNumericEvent(int @event) => lastNumericEvent = @event;
+	public override void HandleNumericEvent(int @event) {
+		lastNumericEvent = @event;
+		eventsRegistered++;
+	}
 
 }
