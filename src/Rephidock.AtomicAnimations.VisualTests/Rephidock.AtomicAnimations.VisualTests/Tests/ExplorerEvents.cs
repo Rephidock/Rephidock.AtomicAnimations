6ec50d9338044ac22828d8c378ff98847850a6b2
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-19 00:36:04 +0200

    Create ExplorerEvents.cs

---
+++ src/Rephidock.AtomicAnimations.VisualTests/Rephidock.AtomicAnimations.VisualTests/Tests/ExplorerEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SFML.System;
+
+
+namespace Rephidock.AtomicAnimations.VisualTests.Tests;
+
+
+[VisualTestMeta(Name = "Explorer Events")]
+public class ExplorerEvents : VisualTest {
+
+	ArrowDirection? lastArrowDirection = null;
+	int? lastNumericEvent = null;
+
+	public override void Start(TimeSpan startTime) { }
+
+	public override void Update(TimeSpan deltaTime) { }
+
+	public override void Draw(Drawer drawer) {
+
+		string lastDirectionalString = lastArrowDirection.HasValue ? lastArrowDirection.Value.ToString() : "None";
+		string lastNumericString = lastNumericEvent.HasValue ? lastNumericEvent.Value.ToString() : "None";
+
+		drawer.DrawText($"Last Directional: {lastDirectionalString}", new Vector2f(100, 100));
+		drawer.DrawText($"Last Numeric: {lastNumericString}", new Vector2f(100, 100 + drawer.MainFontLineSpacing));
+	}
+
+	public override void HandleDirectionEvent(ArrowDirection @event) => lastArrowDirection = @event;
+
+	public override void HandleNumericEvent(int @event) => lastNumericEvent = @event;
+
+}
