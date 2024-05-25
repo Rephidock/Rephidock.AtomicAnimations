3ab787a03a4986313583ba7d98db074895681027
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-05-25 19:20:05 +0200

    Create TestWaveAsEasing.cs

---
+++ src/Rephidock.AtomicAnimations.VisualTests/Tests/TestWaveAsEasing.cs
@@ -0,0 +1,62 @@
+﻿using SFML.System;
+using SFML.Graphics;
+using Rephidock.AtomicAnimations.Waves;
+
+
+namespace Rephidock.AtomicAnimations.VisualTests.Tests;
+
+
+[VisualTestMeta(Name = "Wave as easing (shift)")]
+public class TestWaveAsEasingShift : TestShift2D {
+
+	protected Wave easingWave;
+
+	public TestWaveAsEasingShift() {
+
+		easingWave = new WaveBuilder()
+			.Add(Easing.Linear).To(0.5f).Over(20)
+			.Add(Easing.BounceOut).To(0).Over(100)
+			.Add(Easing.BackOut).To(1).Over(100)
+			.NormalizeWidth()
+			.ToWave();
+
+		curX = startX;
+		curY = startY;
+
+		Anim = new Shift2D(
+			shiftX, shiftY,
+			duration,
+			easingWave.GetValueAt,
+			(x, y) => {
+				curX += x;
+				curY += y;
+			}
+		);
+
+	}
+
+}
+
+
+[VisualTestMeta(Name = "Wave as easing (move)")]
+public class TestWaveAsEasingMove : TestWaveAsEasingShift {
+
+	public TestWaveAsEasingMove() : base() {
+
+		curX = 0;
+		curY = 0;
+
+		Anim = new Move2D(
+			startX, startY,
+			startX + shiftX, startY + shiftY,
+			duration,
+			easingWave.GetValueAt,
+			(x, y) => {
+				curX = x;
+				curY = y;
+			}
+		);
+
+	}
+
+}
